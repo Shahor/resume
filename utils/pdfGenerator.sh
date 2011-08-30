109c57a5585540f22e09b3656641ff012b5409aa
Shahor <shahor@shahor.fr>
2011-08-30 12:39:38 +0200

    Pdf generator
    * Fix for Os X

--- utils/pdfGenerator.sh
+++ utils/pdfGenerator.sh
@@ -19,7 +19,20 @@ if [[ $error ]]
 then exit
 fi
 
-echo "Generating FR version"
-xvfb-run -a -s "-screen 0 ${height}x${width}x16" wkhtmltopdf --dpi 300  --page-size A4 index.html files/resume.pdf
-echo "Generating UK version"
-xvfb-run -a -s "-screen 0 ${height}x${width}x16" wkhtmltopdf --dpi 300  --page-size A4 index_en.html files/resume_en.pdf
+if [[ $platform == 'Linux' ]]
+then
+	echo "Generating FR version"
+	xvfb-run -a -s "-screen 0 ${height}x${width}x16" wkhtmltopdf --dpi 300  --page-size A4 index.html files/resume.pdf
+	echo "Generating UK version"
+	xvfb-run -a -s "-screen 0 ${height}x${width}x16" wkhtmltopdf --dpi 300  --page-size A4 index_en.html files/resume_en.pdf
+elif [[ $platform == 'Darwin' ]]
+then
+	echo "Generating FR version"
+	wkhtmltopdf --dpi 300  --page-size A4 index.html files/resume.pdf
+
+	echo "Generating UK version"
+	wkhtmltopdf --dpi 300  --page-size A4 index_en.html files/resume_en.pdf
+else
+	echo "Who are you ?"
+fi
+
